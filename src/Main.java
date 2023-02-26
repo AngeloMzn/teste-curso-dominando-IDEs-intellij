3e1e93253e060eda289eb113f87c37eb3924db7f
Angelo Mazarin <angelotrab22@gmail.com>
2023-02-25 22:38:23 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,45 @@ public class Main {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+        Livro livro = new Livro("Como fazer Cuscuz", 200);
+        System.out.println(livro);
+    }
+}
 
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public Livro() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
     }
 }
